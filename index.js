37fb43a055dc0fe6777b75d3d66721586a2e4a8d
SidneyStablein <156612706+SidneyStablein@users.noreply.github.com>
2024-02-24 21:07:26 +0000

    Updated index.js and CSS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Add JavaScript below
 window.onload = function() {
     ScrollReveal().reveal('.first', {delay: 1000});
-    ScrollReveal().reveal('.second');
-    ScrollReveal().reveal('.third');
-    ScrollReveal().reveal('.fourth');
+    ScrollReveal().reveal('.second', { origin: 'right'});
+    ScrollReveal().reveal('.third', { easing: 'cubic-bezier(0.5, 0, 0, 1)' });
+    ScrollReveal().reveal('.fourth', { rotate: {x:0, y:0, z:20}});
     ScrollReveal().reveal('.fifth');
     ScrollReveal().reveal('.sixth');
-    ScrollReveal().reveal('.seventh');
+    ScrollReveal().reveal('.seventh', { rotate: {x:0, y:0, z:-20}});
     ScrollReveal().reveal('.eighth');
     ScrollReveal().reveal('.ninth');
 };
