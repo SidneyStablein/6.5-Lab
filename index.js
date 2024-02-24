0b7d8ac6f493a99f4874996679a349be85eb443e
SidneyStablein <156612706+SidneyStablein@users.noreply.github.com>
2024-02-24 20:25:26 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 // Add JavaScript below
 window.onload = function() {
-    ScrollReveal().reveal('.first');
-};
-
-window.onload = function() {
+    ScrollReveal().reveal('.first', {delay: 1000});
     ScrollReveal().reveal('.second');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.third');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.fourth');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.fifth');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.sixth');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.seventh');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.eighth');
-};
-
-window.onload = function() {
     ScrollReveal().reveal('.ninth');
 };
