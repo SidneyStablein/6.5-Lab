1002210a9317c440ccf64e6c30b7f6428fe0cb81
SidneyStablein <156612706+SidneyStablein@users.noreply.github.com>
2024-02-24 21:57:21 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Add JavaScript below
 window.onload = function() {
-    ScrollReveal().reveal('.first', {delay: 1000});
+    ScrollReveal().reveal('.first', { rotate: {x:0, y:0, z:-50}});
     ScrollReveal().reveal('.second', { origin: 'right'});
-    ScrollReveal().reveal('.third', { easing: 'cubic-bezier(0.5, 0, 0, 1)' });
+    ScrollReveal().reveal('.third', { easing: 'ease-in' });
     ScrollReveal().reveal('.fourth', { rotate: {x:0, y:0, z:20}});
-    ScrollReveal().reveal('.fifth');
-    ScrollReveal().reveal('.sixth');
+    ScrollReveal().reveal('.fifth', { easing: 'steps(5)' });
+    ScrollReveal().reveal('.sixth', { scale: 0.85 });
     ScrollReveal().reveal('.seventh', { rotate: {x:0, y:0, z:-20}});
-    ScrollReveal().reveal('.eighth');
-    ScrollReveal().reveal('.ninth');
+    ScrollReveal().reveal('.eighth', { scale: 0.75 });
+    ScrollReveal().reveal('.ninth', {delay: 1000});
 };
